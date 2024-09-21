99d7fe5f30f62440c5d9fc6e347d42bc75f61de0
houseofbogue <james@jamesbogue.co>
2024-09-21 12:32:08 -0400

    Update embed.js

--- embed.js
+++ embed.js
@@ -1,6 +1,6 @@
 // Load main script
 var script = document.createElement('script');
-script.src = 'https://unpkg.com/@houseofbogue/molecule-simulator@1.0.0/molecule-simulator.js';
+script.src = 'https://cdn.jsdelivr.net/gh/houseofbogue/molecule-simulator.js@main/molecule-simulator.js';
 script.onload = function() {
     if (typeof MoleculeSimulator !== 'undefined') {
         new MoleculeSimulator();
